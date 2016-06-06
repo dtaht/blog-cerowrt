83035309c32d3c159491f157432fb6b3dcc59962
Dave Taht <dave.taht@gmail.com>
2016-06-05 20:32:48 -0700

    nm scan now fixed

---
+++ content/flent/nmfixed/long.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+S0=172.26.130.12
+S1=172.26.130.11
+S2="-H $S0 -H $S0 -H $S1 -H $S1"
+T='ecn-nmfixed-fqcodel-tim'
+# Make sure we're alive
+
+fping -c 3 $S0 $S1
+flent -l 60 --step-size=.05 -H $S0 -H $S0 -H $S0 -H $S0  -t "$T-up" rtt_fair_up
+flent -l 60 --step-size=.05 --swap-up-down -H $S0 -H $S0 -H $S0 -H $S0  -t "$T-down" rtt_fair_up
+flent -l 300 --step-size=.05 --swap-up-down -H $S0 -H $S0 -H $S0 -H $S0  -t "$T-down-long" rtt_fair_up
+flent -l 300 --step-size=.05 -H $S0 -H $S0 -H $S0 -H $S0  -t "$T-up-long" rtt_fair_up
+exit
+flent -l 600 --swap-up-down $S2 -t "$T-down-long" rtt_fair_up
+flent -l 600 $S2 "$T-up-long" rtt_fair_up
+
+
