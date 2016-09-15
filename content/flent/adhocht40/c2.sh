4c44673602d489e406cf458067345caa2a85bd86
dave taht <dave@taht.net>
2016-09-15 10:03:03 -0700

    Some tests using an odroid c2 on the other side of a router.
    Running the test through the router rather than to it,
    results in a much better job of "packing the air" - typical aggregation
    of 10 on the download test vs 5 on the others.

--- content/flent/adhocht40/c2.sh
+++ content/flent/adhocht40/c2.sh
@@ -3,7 +3,7 @@
 F="flent -4 -l 30 --test-parameter=wifi_stats_hosts=root@lite-4 --test-parameter=wifi_stats_interfaces=wlan1 --test-parameter=wifi_stats_stations=68:72:51:48:d7:b5,80:2a:a8:17:1b:1d,80:2a:a8:17:1e:95,80:2a:a8:4a:28:00"
 
 
-for s in lite-3
+for s in c2 lite-3
 do
 for i in 1 2 4 8 12 16 24 48
 do
