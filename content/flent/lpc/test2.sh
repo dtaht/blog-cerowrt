cc25396ad9064d68f82b1ad1e4e82043e41da542
Dave Taht <dave.taht@gmail.com>
2019-09-10 01:53:43 -0700

    Sigh. LPC is bloated too

---
+++ content/flent/lpc/test2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+T="Linux_Plumbers"
+F="flent -x -H flent-fremont.bufferbloat.net -t $T"
+
+#$F --te=download_streams=4 tcp_ndown
+#$F --te=upload_streams=4 --socket-stats tcp_nup
+#$F --te=upload_streams=4 tcp_2up_square
+#$F rrul
+tcpdump -i wlp3s0 -s 128 -w raven.cap &
+$F rrul_be
+killall tcpdump
---
+++ content/flent/lpc/testit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+T="Linux_Plumbers"
+F="flent -x -H flent-fremont.bufferbloat.net -t $T"
+
+$F --te=download_streams=4 tcp_ndown
+$F --te=upload_streams=4 --socket-stats tcp_nup
+$F --te=upload_streams=4 tcp_2up_square
+$F rrul
+$F rrul_be
+
